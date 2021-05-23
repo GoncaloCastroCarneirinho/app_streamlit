6ec43d24a0bc1b59f4cdb8770288fce9d6d6685f
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-05-23 22:03:58 +0100

    Updating app.py...

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import streamlit as st
 
 from pygraphtec import  lee_fichero_sesion
 
+import pandas as pd
+
+from collections import deque
+
 from datetime import timedelta, date, time
 
 import datetime
@@ -17,57 +21,81 @@ st.set_page_config(layout="wide")
 
 df = lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')#Se ejecuta la función
 
-def df_filter_date(message, df):#Función para filtrar el Dataframe por fechas
-    
-    filtered_df_date = df.loc[date_1:date_2]#Se filtra el Dataframe df, pasado como argumento,
-                                            #entre los valores date_1 y date_2
+column_1, column_2 = st.beta_columns((1,2))
 
-    return filtered_df_date#Se devuelve el Dataframe filtrado
-    
-def df_filter_time(message, df):#Función para filtrar el Dataframe por horas
-     
-     filtered_df_time = df.loc[time_1:time_2]#Se filtra el Dataframe df, pasado como argumento,
-                                             #entre los valores time_1 y time_2
+i=0
 
-     return filtered_df_time#Se devuelve el Dataframe filtrado
-    
-if __name__ == '__main__':
-    
-    column_1, column_2, column_3 = st.beta_columns(3)#Se estructura la visualización de la 
-                                                     #aplicación mediante columnas del mismo
-                                                     #tamaño
-    with column_1:
-        #Se crea el filtro para seleccionar la primera fecha (de primer dia del df hasta hoy)
-        date_1 = st.date_input('start date', df.index.min(), df.index.min(), datetime.date.today())
-        #Se crea el filtro para seleccionar el primer valor de horario
-        time_1 = st.time_input('start time', time(00,00,00))
+view_mode = column_1.radio("View mode:", ('Live', 'Resume'))
 
-    with column_2:
-        #Se crea el filtro para seleccionar la segunda fecha (del sgundo día del df hasta hoy)
-        date_2 = st.date_input('end date', date_1+datetime.timedelta(days=1), date_1+datetime.timedelta(days=1), datetime.date.today())            
-        #Se crea el filtro para seleccionar el segundo valor de horario
-        time_2 = st.time_input('end time', time(23,30,00))
-        
-    with column_3:
-        #Se crea el filtro para seleccionar las variables a graficar
-        temp_humi = st.multiselect("Select variable to plot: ", df.columns.tolist())
+column_3, column_4 = st.beta_columns([1,1])
+
+column_5, column_6 = st.beta_columns(2) 
+
+if view_mode == 'Resume':
+    
+    date_1 = column_3.date_input('Start date:', df.index.min(), df.index.min(), df.index.max())
+    
+    date_2 = column_4.date_input('End date:', df.index.max(), df.index.min()+timedelta(days=1), datetime.date.today())
+
+    def df_filter_date(message, df):#Función para filtrar el Dataframe por fechas
+    
+        filtered_df_date = df.loc[date_1:date_2]#Se filtra el Dataframe df, pasado como argumento,
+                                                #entre los valores date_1 y date_2
+
+        return filtered_df_date#Se devuelve el Dataframe filtrado
+    
+    filtered_df_date = df_filter_date('Select dates range to filter dataframe',df)
+    
+    temp_humi = column_2.multiselect("Select variable: ", df.columns.tolist())
+    
+    column_5.title('Data Table')
+    column_5.write(filtered_df_date[temp_humi])
+    
+    column_6.title('Data Chart')
+    column_6.line_chart(filtered_df_date[temp_humi])
     
     #Información de las fechas y horarios seleccionados
-    st.info('Start: **%s** at **%s** End: **%s** at **%s**' % (date_1, time_1, date_2, time_2))   
+    st.info('Start: **%s** End: **%s**' % (date_1, date_2))   
  
-    #Filtramos nuestro Dataframe 'df' en fecha y tiempo llamando a las funciones creadas
-    filtered_df_date = df_filter_date('Select dates range to filter dataframe',df)
-    filtered_df_time = df_filter_time('Select times range to filter dataframe',df)
-
-    column_1, column_2 = st.beta_columns(2)#Se estructura la visualización de la aplicación 
-                                           #mediante columnas del mismo tamaño
-
-    with column_1:
-        #Se dibuja la tabla de valores del Dataframe 'df' existente
-        st.title('Data Frame')
-        st.write(filtered_df_date[temp_humi])
-
-    with column_2:
-        #Se grafican las variables previamente seleccionadas
-        st.title('Chart')
-        st.line_chart(filtered_df_date[temp_humi])        
+elif view_mode == 'Live':
+    
+    st.title('Data Chart')
+    
+    temp_humi = column_2.multiselect("Select variable: ", df.columns.tolist())
+    
+    len_=len(temp_humi)
+    data_tail = deque() ###
+    time_tail = deque()
+    
+    column_3.empty()
+    column_3.empty()
+    column_4.empty()
+    column_4.empty()
+    column_5.empty()
+    column_5.empty()
+    column_6.empty() 
+    column_6.empty()
+    
+    i=0
+    
+    while True:
+        if(i<(len(df)-1)):
+            i=i+1
+        
+        data_tail.append(df[temp_humi].values[i]) ##
+        time_tail.append(df.index[i])
+        
+        chart_data = pd.DataFrame(
+             data_tail,
+             index = time_tail,
+             columns = temp_humi
+        )
+        
+        chart = st.line_chart(chart_data)
+        
+        if(temp_humi is None or len(temp_humi)==0):
+            chart = st.line_chart({})
+        else:
+            time.sleep(1)
+            chart.empty()
+            chart.empty()
