3e78835c7eba077da64a23c8d894c69ecbbb7dbd
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-05-25 15:34:36 +0100

    Updating app.py...
    Actualizaciones en la aplicación. Implementación de nuevas características, funcionalidades y corrección de errores.

--- app.py
+++ app.py
@@ -19,19 +19,22 @@ import time
 
 import datetime
 
+import altair as alt
+
 st.set_page_config(layout="wide")
 
-df = lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')#Se ejecuta la función
+#df = lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')#Se ejecuta la función
+df = lee_fichero_sesion("201112-180010.csv", path_sesiones='dataLogger')
 
 column_1, column_2 = st.beta_columns((1,2))
 
 i=0
 
-view_mode = column_1.radio("View mode:", ('Live', 'Resume'))
+view_mode = column_1.radio("View mode:", ('Live', 'Resume', 'Data Table'))
 
 column_3, column_4 = st.beta_columns([1,1])
 
-column_5, column_6 = st.beta_columns(2) 
+column_5, column_6 = st.beta_columns((2,5)) 
 
 if view_mode == 'Resume':
     
@@ -43,15 +46,20 @@ if view_mode == 'Resume':
     
         filtered_df_date = df.loc[date_1:date_2]#Se filtra el Dataframe df, pasado como argumento,
                                                 #entre los valores date_1 y date_2
+                                                
+        #filtered_df_date.index=filtered_df_date.index.strftime("%d/%m - %H:%M")
 
         return filtered_df_date#Se devuelve el Dataframe filtrado
     
     filtered_df_date = df_filter_date('Select dates range to filter dataframe',df)
     
+    df_1=df.loc[date_1:date_2]
+    df_1.index=df_1.index.strftime("%H:%M")
+    
     temp_humi = column_2.multiselect("Select variable: ", df.columns.tolist())
     
     Data_table_title = column_5.title('Data Table')
-    Data_table = column_5.write(filtered_df_date[temp_humi])
+    Data_table = column_5.write(df_1[temp_humi])
     
     Data_chart_title = column_6.title('Data Chart')
     Data_chart = column_6.line_chart(filtered_df_date[temp_humi])
@@ -61,7 +69,11 @@ if view_mode == 'Resume':
  
 elif view_mode == 'Live':
     
-    st.title('Data Chart')
+    #df.index=df.index.strftime("%m/%d/%y\n%H:%M:%S")
+    
+    #column_9, column_10 = st.beta_columns((5,2))
+    
+    #st.title('Data Chart')
     
     temp_humi = column_2.multiselect("Select variable: ", df.columns.tolist())
     
@@ -80,10 +92,12 @@ elif view_mode == 'Live':
     
     i=0
     
+    
+    status_text=st.empty()
     while True:
         if(i<(len(df)-1)):
             i=i+1
-        
+            
         data_tail.append(df[temp_humi].values[i]) ##
         time_tail.append(df.index[i])
         
@@ -93,16 +107,25 @@ elif view_mode == 'Live':
              columns = temp_humi
         )
         
-        column_7, column_8 = st.beta_columns((6,2)) 
+        df1 = pd.DataFrame(
+            [df[temp_humi].values[i]],
+            index = ["At: " + df.index[i].strftime('%d/%m/%y - %H:%M:%S')],
+            columns = temp_humi)
         
         if(temp_humi is None or len(temp_humi)==0):
             time.sleep(1) #necesario para que la aplicación arranque sin dar error de 
                           #'empty chart'
             chart = st.line_chart({})
         else:
-            chart = column_7.line_chart(chart_data)
-            data_table = column_8.dataframe(chart_data)
-            time.sleep(20)
+            #status_text.text(str(temp_humi) + str(df[temp_humi].values[i]))
+            chart = st.line_chart(chart_data)
+            status_text.table(df1)
+            #data_table = column_8.table(df)
+            time.sleep(3)
             chart.empty()
             chart.empty()
-            data_table.empty()
+            #data_table.empty()
+            
+elif view_mode == 'Data Table':
+    df.index=df.index.strftime("%m/%d/%y\n%H:%M:%S")
+    data_table = st.table(df)
