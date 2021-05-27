fc405abcf8913f5bee321907b21a28b568f77e20
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-05-27 01:00:42 +0100

    Updating app.py...
    View mode control updated

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import time
 
 import datetime
 
-import altair as alt
+from threading import Event
 
 st.set_page_config(layout="wide")
 
@@ -44,10 +44,8 @@ if view_mode == 'Resume':
 
     def df_filter_date(message, df):#Función para filtrar el Dataframe por fechas
     
-        filtered_df_date = df.loc[date_1:date_2]#Se filtra el Dataframe df, pasado como argumento,
-                                                #entre los valores date_1 y date_2
-                                                
-        #filtered_df_date.index=filtered_df_date.index.strftime("%d/%m - %H:%M")
+        filtered_df_date = df.loc[date_1:date_2]#Se filtra el Dataframe df, pasado como 
+                                                #argumento entre los valores date_1 y date_2
 
         return filtered_df_date#Se devuelve el Dataframe filtrado
     
@@ -67,18 +65,12 @@ if view_mode == 'Resume':
     #Información de las fechas y horarios seleccionados
     #st.info('Start: **%s** End: **%s**' % (date_1, date_2))   
  
-elif view_mode == 'Live':
-    
-    #df.index=df.index.strftime("%m/%d/%y\n%H:%M:%S")
-    
-    #column_9, column_10 = st.beta_columns((5,2))
-    
-    #st.title('Data Chart')
+if view_mode == 'Live':
     
     temp_humi = column_2.multiselect("Select variable: ", df.columns.tolist())
     
     len_=len(temp_humi)
-    data_tail = deque() ###
+    data_tail = deque()
     time_tail = deque()
     
     column_3.empty()
@@ -91,10 +83,13 @@ elif view_mode == 'Live':
     column_6.empty()
     
     i=0
-    
-    
+
     status_text=st.empty()
-    while True:
+    
+    while True and view_mode=='Live':
+        
+        j=0
+        
         if(i<(len(df)-1)):
             i=i+1
             
@@ -119,13 +114,18 @@ elif view_mode == 'Live':
         else:
             #status_text.text(str(temp_humi) + str(df[temp_humi].values[i]))
             chart = st.line_chart(chart_data)
-            status_text.table(df1)
-            #data_table = column_8.table(df)
-            time.sleep(3)
+            status_text.table(df1.style.set_properties(**{'font-size': '15px','text-align': 'right'}).set_precision(2))
+            
+            for j in range(10):
+                time.sleep(1)
+                st.empty()
+                
             chart.empty()
             chart.empty()
             #data_table.empty()
             
-elif view_mode == 'Data Table':
+if view_mode == 'Data Table':
+    view_mode='Data Table'
+    
     df.index=df.index.strftime("%m/%d/%y\n%H:%M:%S")
     data_table = st.table(df)
