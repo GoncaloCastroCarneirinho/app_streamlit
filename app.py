20b996af28929ae061e98e15b3d5f54ebd1855f4
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-08-19 00:00:42 +0100

    actualizaciones app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
         
         with st.beta_expander("Pick a dataset to plot:"): #CREACIÓN DE FILTRO O PESTAÑA DESPLEGABLE PARA GRÁFICO 1
             
-            set_option_1 = st.selectbox('', ['DATASET ALREADY SELECTED','TEMPERATURE - MOCKUPS 1 & 2 - DATALOGGER','RELATIVE HUMIDITY - MOCKUPS 1 & 2 - DATALOGGER','ATMOSPHERIC PRESSURE - MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE & RELATIVE HUMIDITY - ELECTRIC CABINET - DATALOGGER','TEMPERATURE - FRONTSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - GEONICA','RELATIVE HUMIDITY - GEONICA','CELULAS TOP, MID & BOT - GEONICA','IRRADIANCE - GEONICA','WIND SPEED & DIRECTION - GEONICA','SUN ELEVATION & ORIENTATION - GEONICA','IRRADIANCE & TEMPERATURE PIRGEO - GEONICA','ATMOSPHERIC PRESSURE - GEONICA','PRECIPITATION - GEONICA']) #OPCIONES DE SETS DE DATOS A REPRESENTAR EN GRÁFICO 1
+            set_option_1 = st.selectbox('', ['DATASET ALREADY SELECTED','TEMPERATURE - MOCKUPS 1 & 2 - DATALOGGER','RELATIVE HUMIDITY - MOCKUPS 1 & 2 - DATALOGGER','PRESSURE - MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE & RELATIVE HUMIDITY - ELECT. CABIN. - DATALOGGER','TEMPERATURE - FRONTSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - GEONICA','RELATIVE HUMIDITY - GEONICA','CELULAS TOP, MID & BOT - GEONICA','IRRADIANCE - GEONICA','WIND SPEED & DIRECTION - GEONICA','SUN ELEVATION & ORIENTATION - GEONICA','IRRADIANCE & TEMPERATURE PIRGEO - GEONICA','ATMOSPHERIC PRESSURE - GEONICA','PRECIPITATION - GEONICA']) #OPCIONES DE SETS DE DATOS A REPRESENTAR EN GRÁFICO 1
 
             if set_option_1 == "DATASET ALREADY SELECTED":
                 df_set_filter_1 = df[first_variables_set_selected_resume]
@@ -147,7 +147,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
                 df_set_filter_1 = df[[item for item in df.columns if 'M1-SP' in item]+
                                      [item for item in df.columns if 'M2-SP' in item]] #DATAFRAME INICIAL, FILTRADO POR PRESIONES ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
 
-            elif set_option_1 == "TEMPERATURE & RELATIVE HUMIDITY - ELECTRIC CABINET - DATALOGGER":
+            elif set_option_1 == "TEMPERATURE & RELATIVE HUMIDITY - ELECT. CABIN. - DATALOGGER":
                 df_set_filter_1 = df[[item for item in df.columns if 'C-TEMP' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
                 
             elif set_option_1 == "TEMPERATURE - FRONTSIDE MOCKUPS 1 & 2 - DATALOGGER":
@@ -156,7 +156,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
                 
             elif set_option_1 == "TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER":
                 df_set_filter_1 = df[[item for item in df.columns if 'M1-Tp BS' in item]+
-                                     [item for item in df.columns if 'M2-Tp BS' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
+                                     [item for item in df.columns if 'M2-TP BS' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
 
             elif set_option_1 == "ATMOSPHERIC TEMPERATURE - GEONICA":
                 df_set_filter_1 = df[[item for item in df.columns if 'Temp. Ai' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DE GEÓNICA
@@ -196,7 +196,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
             
         with st.beta_expander("Pick a dataset to plot:"): #CREACIÓN DE FILTRO O PESTAÑA DESPLEGABLE PARA GRÁFICO 2
             
-            set_option_2 = st.selectbox('', ['DATASET ALREADY SELECTED ','TEMPERATURE - MOCKUPS 1 & 2 - DATALOGGER','RELATIVE HUMIDITY - MOCKUPS 1 & 2 - DATALOGGER','ATMOSPHERIC PRESSURE - MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE & RELATIVE HUMIDITY - ELECTRIC CABINET - DATALOGGER','TEMPERATURE - FRONTSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER','ATMOSPHERIC TEMPERATURE - GEONICA','RELATIVE HUMIDITY - GEONICA','CELULAS TOP, MID & BOT - GEONICA','IRRADIANCE - GEONICA','WIND SPEED & DIRECTION - GEONICA','SUN ELEVATION & ORIENTATION - GEONICA','IRRADIANCE & TEMPERATURE PIRGEO - GEONICA','ATMOSPHERIC PRESSURE - GEONICA','PRECIPITATION - GEONICA']) #OPCIONES DE SETS DE DATOS A REPRESENTAR EN GRÁFICO 2
+            set_option_2 = st.selectbox('', ['DATASET ALREADY SELECTED ','TEMPERATURE - MOCKUPS 1 & 2 - DATALOGGER','RELATIVE HUMIDITY - MOCKUPS 1 & 2 - DATALOGGER','PRESSURE - MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE & RELATIVE HUMIDITY - ELECT. CABIN. - DATALOGGER','TEMPERATURE - FRONTSIDE MOCKUPS 1 & 2 - DATALOGGER','TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER','ATMOSPHERIC TEMPERATURE - GEONICA','RELATIVE HUMIDITY - GEONICA','CELULAS TOP, MID & BOT - GEONICA','IRRADIANCE - GEONICA','WIND SPEED & DIRECTION - GEONICA','SUN ELEVATION & ORIENTATION - GEONICA','IRRADIANCE & TEMPERATURE PIRGEO - GEONICA','ATMOSPHERIC PRESSURE - GEONICA','PRECIPITATION - GEONICA']) #OPCIONES DE SETS DE DATOS A REPRESENTAR EN GRÁFICO 2
             
             if set_option_2 == "DATASET ALREADY SELECTED ":
                 df_set_filter_2 = df[second_variables_set_selected_resume] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS SEGÚN SET DE VARIABLES SELECCIONADO MANUALMENTE POR USUARIOS
@@ -213,7 +213,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
                 df_set_filter_2 = df[[item for item in df.columns if 'M1-SP' in item]+
                                      [item for item in df.columns if 'M2-SP' in item]] #DATAFRAME INICIAL, FILTRADO POR PRESIONES ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
 
-            elif set_option_2 == "TEMPERATURE & RELATIVE HUMIDITY - ELECTRIC CABINET - DATALOGGER":
+            elif set_option_2 == "TEMPERATURE & RELATIVE HUMIDITY - ELECT. CABIN. - DATALOGGER":
                 df_set_filter_2 = df[[item for item in df.columns if 'C-TEMP' in item]+
                                      [item for item in df.columns if 'C-RH' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
 
@@ -223,7 +223,7 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
 
             elif set_option_2 == "TEMPERATURE - BACKSIDE MOCKUPS 1 & 2 - DATALOGGER":
                 df_set_filter_2 = df[[item for item in df.columns if 'M1-Tp BS' in item]+
-                                     [item for item in df.columns if 'M2-Tp BS' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
+                                     [item for item in df.columns if 'M2-TP BS' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DEL INTERIOR DE LOS MOCKUPS DEL DATALOGGER
 
             elif set_option_2 == "ATMOSPHERIC TEMPERATURE - GEONICA":
                 df_set_filter_2 = df[[item for item in df.columns if 'Temp. Ai' in item]] #DATAFRAME INICIAL, FILTRADO POR TEMPERATURAS ATMOSFÉRICAS DE GEÓNICA
@@ -277,17 +277,17 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
     
     df_filter_chart2=df_set_filter_2.loc[fecha_inicio_resume:fecha_fin_resume] #DATAFRAME FILTRADO POR FECHAS (GRÁFICO 2) - LLAMADA A LA FUNCIÓN
     
-    #chart1_title = grafico1_resume.markdown("<p style='display: block; text-align: center; font-size: 15px; font-family: calibri; font-weight: bold'>DATASET 1 RESUME</p>", unsafe_allow_html=True,) #TÍTULO DE GRÁFICO 1
+    chart1_title = grafico1_resume.markdown("<p style='display: block; text-align: center; font-size: 14px; font-family: calibri; font-weight: bold'>"+set_option_1, unsafe_allow_html=True,) #TÍTULO DE GRÁFICO 1
     
     Data_chart1 = grafico1_resume.empty() #CREACIÓN DE GRÁFICO 1, INICIALMENTE VACÍO
     
-    Data_chart01 = Data_chart1.line_chart(df_filter_chart1) #REPRESENTACIÓN GRÁFICA DE DATAFRAME 1
+    Data_chart01 = Data_chart1.line_chart(df_filter_chart1, height=280) #REPRESENTACIÓN GRÁFICA DE DATAFRAME 1
 
-    #chart2_title = grafico2_resume.markdown("<p style='display: block; text-align: center; font-size: 15px; font-family: calibri; font-weight: bold'>DATASET 2 RESUME</p>", unsafe_allow_html=True,)
+    chart2_title = grafico2_resume.markdown("<p style='display: block; text-align: center; font-size: 14px; font-family: calibri; font-weight: bold'>"+set_option_2, unsafe_allow_html=True,)
     
     Data_chart2 = grafico2_resume.empty() #CREACIÓN DE GRÁFICO 2, INICIALMENTE VACÍO
     
-    Data_chart02 = Data_chart2.line_chart(df_filter_chart2) #REPRESENTACIÓN GRÁFICA DE DATAFRAME 2
+    Data_chart02 = Data_chart2.line_chart(df_filter_chart2,  height=280) #REPRESENTACIÓN GRÁFICA DE DATAFRAME 2
      
 elif view_mode == 'Data Table':
     
