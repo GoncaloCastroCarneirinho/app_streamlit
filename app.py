7cf9f20d4aaafe29ccaf2437e3dd69cd8f611549
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-09-01 02:01:32 +0200

    actualizaciones app

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(f"""<style>.reportview-container {{
         background-blend-mode: lighten;
         }}</style>""",
         unsafe_allow_html=True) #FONDO DE LA APLICACIÓN
-    
+
 st.markdown("""<style>footer {
         display:block; 
         text-align:center;
@@ -35,15 +35,16 @@ st.markdown("""<style>footer {
         font-size:11px;
         color:white;
         }</style>""", 
-        unsafe_allow_html=True) #DERECHOS DE LA APLICACIÓN
+        unsafe_allow_html=True) #DERECHOS DE IMAGEN DE LA APLICACIÓN
 
 #df = lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')#Se ejecuta la función
-print(lee_fichero_sesion(name, path_sesiones='dataLogger') for name in glob.glob("*.csv"))
+path="dataLogger/"
+print(glob.glob(path + "*.csv"))
 
-#df_datalogger = pd.concat(lee_fichero_sesion(name, path_sesiones="dataLogger") for name in glob.glob("*.csv"))
-#df_datalogger = pd.concat([lee_fichero_sesion("201112-180010.csv", path_sesiones='dataLogger'), lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')],axis=0)#DATAFRAME DE FICHERO DATALOGGER
-df_datalogger = lee_fichero_sesion("201112-180010.csv", path_sesiones='dataLogger')#Se ejecuta la función
-df_meteo = lee_meteo(pd.date_range(start='2020/11/12', end='2020/11/16', freq='10T'),path_estacion="dataLogger/") #DATAFRAME DE FICHERO METEO
+df_datalogger = pd.concat(lee_fichero_sesion(name, path_sesiones="") for name in glob.glob("dataLogger/*.csv"))
+#df_datalogger = pd.concat([lee_fichero_sesion("201112-180010.csv", path_sesiones='dataLogger'), lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')])#DATAFRAME DE FICHERO DATALOGGER
+#df_datalogger = lee_fichero_sesion("201112-180010.csv", path_sesiones='dataLogger')#Se ejecuta la función
+df_meteo = lee_meteo(pd.date_range(start='2020/11/12', end='2020/11/16', freq='1T'),path_estacion="dataLogger/") #DATAFRAME DE FICHERO METEO
 
 df_meteo = lee_meteo(df_datalogger.index.round('T'),path_estacion="dataLogger/")
 df_meteo.index = df_datalogger.index
@@ -97,7 +98,7 @@ if view_mode == 'Live': #MODALIDAD 'LIVE'
             
         #ACTUALIZACIÓN DE DATOS EN COLECCIONES
         data_collection.append(df[magnitudes_seleccion].values[i]) #RELLENO DE data_collection CON VALORES DE DATOS DE DATAFRAME INICIAL
-        time_collection.append(df.index[i]) #RELLENO DE time_collection CON INFORMACIÓN DE ÍNDICE DE DATAFRAME INICIAL
+        time_collection.append(df.index[i]+timedelta(hours=1)) #RELLENO DE time_collection CON INFORMACIÓN DE ÍNDICE DE DATAFRAME INICIAL
         
         #DATAFRAME PARA REPRESENTACIÓN GRÁFICA EN TIEMPO REAL
         chart_dataframe = pd.DataFrame(
@@ -260,12 +261,14 @@ elif view_mode == 'Resume': #MODALIDAD 'RESUME'
     
     #GRÁFICO 1
     df_filter_chart1=df_set_filter_1.loc[fecha_inicio_resume:fecha_fin_resume] #DATAFRAME FILTRADO POR FECHAS
+    df_filter_chart1.index=df_filter_chart1.index+timedelta(hours=1)
     #chart1_title = grafico1_resume.markdown("<p style='display: block; text-align: center; font-size: 14px; font-family: calibri; font-weight: bold'>"+set_option_1, unsafe_allow_html=True,) #TÍTULO DE GRÁFICO 1
     #chart1 = grafico1_resume.empty() #CREACIÓN DE GRÁFICO, INICIALMENTE VACÍO
     Data_chart1 = grafico1_resume.line_chart(df_filter_chart1, use_container_width=True) #REPRESENTACIÓN GRÁFICA DE DATAFRAME
 
     #GRÁFICO 2
     df_filter_chart2=df_set_filter_2.loc[fecha_inicio_resume:fecha_fin_resume] #DATAFRAME 2 FILTRADO POR FECHAS
+    df_filter_chart2.index=df_filter_chart2.index+timedelta(hours=1)
     #chart2_title = grafico2_resume.markdown("<p style='display: block; text-align: center; font-size: 14px; font-family: calibri; font-weight: bold'>"+set_option_2, unsafe_allow_html=True,)
     #chart2 = grafico2_resume.empty() #CREACIÓN DE GRÁFICO, INICIALMENTE VACÍO
     Data_chart2 = grafico2_resume.line_chart(df_filter_chart2, use_container_width=True) #REPRESENTACIÓN GRÁFICA DE DATAFRAME
